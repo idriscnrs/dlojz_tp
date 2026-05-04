0ed2db0f09a25c8cd74432e21f5c66aa19960046
mantegazza <leo.mantegazza@idris.fr>
2026-05-04 17:42:48 +0200

    MAJ GradScaler et autocast pour eviter deprecation

--- Jour2/1.tp_dlojz_jour2/dlojz2_2.py
+++ Jour2/1.tp_dlojz_jour2/dlojz2_2.py
@@ -22,7 +22,7 @@ np.random.seed(123)
 torch.manual_seed(123)                                                                                        
 
 ## import ... ## Add here the libraries to import
-from torch.cuda.amp import autocast, GradScaler
+from torch.amp import autocast, GradScaler
 #TODO: import libraries related to distribution
 
 
@@ -114,7 +114,7 @@ def train():
     val_metric['acc'] = MulticlassAccuracy(num_classes=1000, average='micro').to(gpu)
     
     # Creates a GradScaler once at the beginning of training.
-    scaler = GradScaler()
+    scaler = GradScaler('cuda')
         
 
     #########  DATALOADER ############ 
@@ -207,7 +207,7 @@ def train():
 
             optimizer.zero_grad()
             # Implement autocasting
-            with autocast():
+            with autocast('cuda'):
                 outputs = model(images)
                 loss = criterion(outputs, labels)
             
@@ -260,7 +260,7 @@ def train():
                     # Runs the forward pass with no grad mode.
                     with torch.no_grad():
                         # Implement autocasting
-                        with autocast():
+                        with autocast('cuda'):
                             val_outputs = model(val_images)
                             val_loss = criterion(val_outputs, val_labels)
 
--- Jour2/1.tp_dlojz_jour2/dlojz2_3_2.py
+++ Jour2/1.tp_dlojz_jour2/dlojz2_3_2.py
@@ -119,7 +119,7 @@ def train():
     val_metric['acc'] = MulticlassAccuracy(num_classes=1000, average='micro').to(gpu)
     
     # Creates a GradScaler once at the beginning of training.
-    scaler = GradScaler()
+    scaler = GradScaler('cuda')
         
 
     #########  DATALOADER ############ 
@@ -222,7 +222,7 @@ def train():
 
             optimizer.zero_grad()
             # Implement autocasting
-            with autocast():
+            with autocast('cuda'):
                 outputs = model(images)
                 loss = criterion(outputs, labels)
             
@@ -267,7 +267,7 @@ def train():
                     # Runs the forward pass with no grad mode.
                     with torch.no_grad():
                         # Implement autocasting
-                        with autocast():
+                        with autocast('cuda'):
                             val_outputs = model(val_images)
                             val_loss = criterion(val_outputs, val_labels)
 
