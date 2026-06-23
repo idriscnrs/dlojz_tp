fc149695822be7d7c4619f773b3c0d723c4a8cab
mantegazza <leo.mantegazza@idris.fr>
2026-06-23 17:37:33 +0200

    updated DA solutions

--- Jour2/2.tp_data_augmentation/solutions/dlojz_da_1.py
+++ Jour2/2.tp_data_augmentation/solutions/dlojz_da_1.py
@@ -10,19 +10,19 @@ from torch.utils.checkpoint import checkpoint_sequential
 import torch                                                                                                  
 import numpy as np                                     
 import apex
-                                                                                                              
+
 import idr_torch                                                                                              
 from dlojz_chrono import Chronometer
 from torchmetrics.aggregation import MeanMetric
 from torchmetrics.classification import MulticlassAccuracy
-                                                                                                              
+
 import random                                                                                                 
 random.seed(123)                                                                                              
 np.random.seed(123)                                                                                           
 torch.manual_seed(123)                                                                                        
 
 ## import ... ## Add here the libraries to import
-from torch.cuda.amp import autocast, GradScaler
+from torch.amp import autocast, GradScaler
 import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel
 
@@ -111,7 +111,7 @@ def train():
     val_metric['acc'] = MulticlassAccuracy(num_classes=1000, average='micro').to(gpu)
     
     # Creates a GradScaler once at the beginning of training.
-    scaler = GradScaler()
+    scaler = GradScaler('cuda')
         
 
     #########  DATALOADER ############ 
@@ -213,7 +213,7 @@ def train():
 
             optimizer.zero_grad()
             # Implement autocasting
-            with autocast():
+            with autocast('cuda'):
                 outputs = model(images)
                 loss = criterion(outputs, labels)
             
@@ -259,7 +259,7 @@ def train():
                     # Runs the forward pass with no grad mode.
                     with torch.no_grad():
                         # Implement autocasting
-                        with autocast():
+                        with autocast('cuda'):
                             val_outputs = model(val_images)
                             val_loss = criterion(val_outputs, val_labels)
 
@@ -283,7 +283,8 @@ def train():
             #### END OF VALIDATION ############
             
             if args.test: chrono.next_iter()
-    
+
+    dist.destroy_process_group()
                                                              
     chrono.stop()
     if idr_torch.rank == 0:
